563e91aff256595308b1fe32a77e9a53406d1957
jjchz <u201519930@upc.edu.pe>
2015-11-09 04:24:10 +0000

    add module of security (Login and register)

--- taxiweb/db/schema.rb
+++ taxiweb/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108184628) do
+ActiveRecord::Schema.define(version: 20151108231838) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name", limit: 255
+    t.string   "last_name",  limit: 255
+    t.datetime "born_at"
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,4 +42,5 @@ ActiveRecord::Schema.define(version: 20151108184628) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "users"
 end
