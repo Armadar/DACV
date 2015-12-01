252c16611e8ac09c1aba89c39f8b7dfb25c1079f
jjchz <u201519930@upc.edu.pe>
2015-12-01 02:25:00 +0000

    test change 3

--- taxiweb/app/models/service.rb
+++ taxiweb/app/models/service.rb
@@ -1,6 +1,31 @@
 class Service < ActiveRecord::Base
+  attr_accessor :driverName, :userName
   belongs_to :user
   
+    def driverName
+      @driverName
+    end
+    
+    def userName
+      @userName
+    end
+    
+    def dayFormat
+      today = Time.new.in_time_zone('US/Pacific')
+      date =''
+      
+      if self.day.strftime("%d") == today.strftime("%d")
+        date = "Today " + self.day.strftime("%H:%M")
+      else
+        date = self.day.strftime("%d-%m  %H:%M")
+      end
+        date
+    end
+    
+    def hasDriver
+      self.driver==nil
+    end
+
   def hourservice
       time = self.day
       hour_aux = time.hour
@@ -31,9 +56,6 @@ class Service < ActiveRecord::Base
   def customerName
       myProfile = Profile.find_by(user_id: self.user_id)
       myProfile.first_name + " " + myProfile.last_name
-  end
-  
-  def photoProfile
   end
   
 end
